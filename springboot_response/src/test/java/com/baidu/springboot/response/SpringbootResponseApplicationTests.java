5dc01f17b6509c55fc361c4904515cdde915523b
yanxiangzhou <15337121992@163.com>
2025-05-10 20:37:22 +0800

    feature: 【初始化项目结构】

---
+++ springboot_response/src/test/java/com/baidu/springboot/response/SpringbootResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baidu.springboot.response;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootResponseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
